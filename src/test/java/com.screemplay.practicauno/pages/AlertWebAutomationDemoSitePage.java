77471fa43937ca0b63295b8b0b1ace6c311ca34b
jorgeyanquen <jorgeyanquen63@gmail.com>
2024-08-06 14:47:35 -0500

    version 2 alertas

--- src/test/java/com.screemplay.practicauno/pages/AlertWebAutomationDemoSitePage.java
+++ src/test/java/com.screemplay.practicauno/pages/AlertWebAutomationDemoSitePage.java
@@ -4,6 +4,9 @@ import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 import org.openqa.selenium.support.FindBy;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
 public class AlertWebAutomationDemoSitePage extends PageObject {
 
     @FindBy(xpath = "//*[@id=\"header\"]/nav/div/div[2]/ul/li[4]/a")
@@ -15,6 +18,19 @@ public class AlertWebAutomationDemoSitePage extends PageObject {
     @FindBy(xpath = "//*[@id=\"OKTab\"]/button")
     public WebElementFacade btnAlert1;
 
+    @FindBy(xpath = "/html/body/div[1]/div/div/div/div[1]/ul/li[2]/a")
+    public WebElementFacade btnopcion2;
+
+    @FindBy(xpath = "//*[@id=\"CancelTab\"]/button")
+    public WebElementFacade btnopciondosuno;
+
+    @FindBy(xpath = "//*[@id=\"Textbox\"]/button")
+    public WebElementFacade btnopciontres;
+
+    //label a verificar
+    @FindBy(id = "demo1")
+    public WebElementFacade lblHomePpal;
+
 
     public void botonswitch(){
        //Ingreso al menu
@@ -23,5 +39,13 @@ public class AlertWebAutomationDemoSitePage extends PageObject {
 //Automatizacion primera alerta
         btnAlert1.click();
         getDriver().switchTo().alert().accept();
+
+        //Automatizacion opcion alerta ok o cancelar
+        btnopcion2.click();
+        btnopciondosuno.click();
+        getDriver().switchTo().alert().dismiss();
+
+      
+
     }
 }
